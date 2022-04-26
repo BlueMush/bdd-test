dd29a7448da927db2a813e550b2b4fce2bb6b191
BlueMush <gusah0909@naver.com>
2022-04-26 14:27:42 +0900

    코드 분석

--- src/main/java/com/example/demo/mock/study/StudyService.java
+++ src/main/java/com/example/demo/mock/study/StudyService.java
@@ -9,23 +9,27 @@ import com.example.demo.mock.member.MemberService;
 public class StudyService {
     private final MemberService memberService;
 
-    private final StudyRepository repository;
+    private final StudyRepository studyRepository;
 
-    public StudyService(MemberService memberService, StudyRepository repository) {
+    public StudyService(MemberService memberService, StudyRepository studyRepository) {
     	assert memberService != null;
-    	assert repository != null;
+    	assert studyRepository != null;
         this.memberService = memberService;
-        this.repository = repository;
+        this.studyRepository = studyRepository;
     }
 
-    public Study createNewStudy(Long memberId, Study study) {		//	매개변수 1L, study객체 전달 
+    public Study createNewStudy(Long memberId, Study study) {				//	매개변수 1L, study객체 전달 
         Optional<Member> member = memberService.findById(memberId);			//	memberService class의 findById 메서드에 1L값이 호출되면
         																	//	StudyService studyService = new StudyService(memberServiceMock, studyRepoMock);
         																	//	지정한 동작을 수행하여 member 객체가 리턴됨
-        study.setOwner(member.orElseThrow(()->new IllegalArgumentException("Member doesn't exist for id: '" + memberId + "'")));
         
-        Study newStudy = repository.save(study);
-        memberService.notify(newStudy);
+        study.setOwner(member.orElseThrow(()->												//	study Owner에 member를 set하고 member가 null이면
+        new RuntimeException("Member doesn't exist for id: '" + memberId + "'")));			//	RuntimeException을 발생
+        
+        Study newStudy = studyRepository.save(study);		//	
+        memberService.notify(newStudy);						//	
+        
+        
         
         return newStudy;
     }
--- src/test/java/com/example/demo/mock/createStudyService.java
+++ src/test/java/com/example/demo/mock/createStudyService.java
@@ -2,13 +2,20 @@
 package com.example.demo.mock;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -35,17 +42,18 @@ public class createStudyService {
 		Member optMember = new Member();		//	Member 객체 선언
 		optMember.setId(1L);
 		
-		
+		Study study = new Study(10, "java");
 		//	ㅡㅡㅡㅡㅡㅡㅡStubbingㅡㅡㅡㅡㅡㅡㅡ
 		given(memberServiceMock.findById(1L))				//	given MemberService class의 findById가 매개변수로 1L 호출되면
 				.willReturn(Optional.of(optMember))			//	willReturn Null이 아닌(Optional.of인) optMember 객체를 리턴함
 				.willThrow(new RuntimeException());			//	두번째 호출 시 RuntimeException을 던짐
 		
+		given(studyRepoMock.save(study)).willReturn(study);
+		
 		doThrow(new RuntimeException()).when(memberServiceMock).validate(1L);				//	doThrow 던지다 -> RuntimeException을;;
 																							//	when memberService의 validate가 1L 값으로 호출됬을때
 		// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		
-		Study study = new Study(10, "java");
 		studyService.createNewStudy(1L, study);		//	studyService class의 createNewStudy 메서드 첫번째 호출
 		
 		assertThrows(RuntimeException.class, ()-> {
@@ -56,6 +64,9 @@ public class createStudyService {
 			memberServiceMock.validate(1L);				//	memberService interface의 validate 메서드 호출
 		});
 		
+		verify(memberServiceMock, times(1)).notify(study);	//	memberservice mock에 notify가 study값을 가지고 한번 호출되어야 하는것을 검증
+		verify(memberServiceMock, never()).validate(2L);	//	2L 값을 가지고 한번도 호출되지 않는지 검증
 		
+		verifyNoMoreInteractions(memberServiceMock);		//	더 이상 mock객체를 사용하지 않는지 검증
 	}
 }
